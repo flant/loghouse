9d20cacddfdfac8fef384a7a77ccceb7f42d6c8d
anton.klimov <anton.klimov@flant.ru>
2017-09-20 05:41:00 +0300

    period search interface

--- public/assets/javascripts/main.js
+++ public/assets/javascripts/main.js
@@ -41,6 +41,35 @@ $(document).ready(function() {
     }
   });
 
+  $('.super-date-picker__quick-item').on('click', function() {
+    // get data
+    var current_item_value_from = $(this).data('from');
+    var current_item_value_to = $(this).data('to');
+    // set data
+    $('#time-from').val(current_item_value_from);
+    $('#time-to').val(current_item_value_to);
+    // set title
+    refreshQuickItemTitle();
+    // update styles
+    $('.super-date-picker__quick-item').removeClass('btn-primary').removeClass('active').addClass('btn-default');
+    $(this).removeClass('btn-default').addClass('btn-primary').addClass('active');
+  });
+
+  $('.input-group.date .input-group-addon').on('click', function() {
+    $('.super-date-picker__quick-item').removeClass('btn-primary').removeClass('active').addClass('btn-default');
+  });
+
+  $('.input-group.date').datetimepicker({locale: 'ru', keepInvalid: true});
+  $('.input-group.date input').on('input', function(e) {refreshQuickItemTitle()})
+  $('.input-group.date').on('dp.change', function(e) {refreshQuickItemTitle()})
+
+  function refreshQuickItemTitle() {
+    var quick_item_selector = '.super-date-picker__quick-item[data-from="' + $('#time-from').val() + '"][data-to="' + $('#time-to').val() + '"]';
+    var quick_item = $(quick_item_selector);
+    var new_quick_title = quick_item.length ? quick_item.text() : $('#time-from').val() + ' – ' + $('#time-to').val();
+    $('.super-date-picker__period-title').text(new_quick_title);
+  }
+
   // $('#time-from').datetimepicker({
   //     locale: 'ru'
   // });
