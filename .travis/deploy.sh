9d4d52bda9d14b9b8418f57e471595f043a2a013
Andrey Sidorov <andrey.sidorov@flant.com>
2017-10-30 23:45:49 +0300

    fix travis CI: install helm and repo index

--- .travis/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git config user.name "Travis CI"
-git config user.email "travis@travis-ci.org"
-helm package charts/loghouse -d charts/
-git checkout gh-pages
-helm repo index charts/
-git add charts/
-git commit -m 'add new version'
-git push --set-upstream origin gh-pages
