d1fa88ea7535920e987e6791ce3ddc412cd8a8f4
Nikolay Bogdanov <30434588+gyrter@users.noreply.github.com>
2019-08-26 12:31:49 +0300

    Useful timeout for csv queries
    Fix timeout for csv queries

--- lib/clickhouse_read_timeout_patch.rb
+++ lib/clickhouse_read_timeout_patch.rb
@@ -2,7 +2,7 @@ module ClickhouseReadTimeoutPatch
   def client
     @client ||= Faraday.new(url: url) do |f|
       f.adapter :net_http do |http|
-        http.read_timeout = ENV.fetch('CLICKHOUSE_READ_TIMEOUT') { '300' }.to_i
+        http.read_timeout = ENV.fetch('CLICKHOUSE_READ_TIMEOUT') { '1800' }.to_i
       end
     end
   end
