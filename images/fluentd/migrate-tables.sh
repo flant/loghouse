d1599109ea1414fe255839dea19fad355223d470
Nikolay Bogdanov <nikolay.bogdanov@flant.com>
2020-03-06 00:06:27 +0300

    Push docs, push fix for migration script

--- images/fluentd/migrate-tables.sh
+++ images/fluentd/migrate-tables.sh
@@ -2,8 +2,17 @@
 
 CLICKHOUSE_CLIENT="clickhouse-client --host=${CLICKHOUSE_URL} --port=${CLICKHOUSE_PORT} -d ${CLICKHOUSE_DATABASE} --user=${CLICKHOUSE_USER} --password=${CLICKHOUSE_PASSWORD} --query"
 
-for table_name in `$CLICKHOUSE_CLIENT "SHOW TABLES" | grep -P "^${CLICKHOUSE_LOGS_TABLE}\d+'`; do
-  echo $table_name
-  $CLICKHOUSE_CLIENT "SELECT * FROM ${table_name} FORMAT TabSeparated" | $CLICKHOUSE_CLIENT "INSERT INTO ${CLICKHOUSE_LOGS_TABLE} FORMAT TabSeparated"
-  $CLICKHOUSE_CLIENT "DROP TABLE IF EXISTS ${table_name}"
-done
+DB_VERSION=`$CLICKHOUSE_CLIENT "SELECT MAX(version) FROM migrations"`
+
+case $DB_VERSION in
+  0|1|2|3)
+    for table_name in `$CLICKHOUSE_CLIENT "SHOW TABLES" | grep -P "^${CLICKHOUSE_LOGS_TABLE}\d+'`; do
+      echo $table_name
+      $CLICKHOUSE_CLIENT "INSERT INTO ${CLICKHOUSE_LOGS_TABLE} SELECT * FROM ${table_name}"
+      $CLICKHOUSE_CLIENT "DROP TABLE IF EXISTS ${table_name}"
+    done
+    ;;
+  *)
+    echo "Unknown db version $DB_VERSION"
+    ;;
+esac
