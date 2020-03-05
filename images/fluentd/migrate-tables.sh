45d86633e05954f0ad965df7f18b7518604d3a28
Nikolay Bogdanov <nikolay.bogdanov@flant.com>
2020-03-06 01:41:23 +0300

    Fix migration script

--- images/fluentd/migrate-tables.sh
+++ images/fluentd/migrate-tables.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-CLICKHOUSE_CLIENT="clickhouse-client --host=${CLICKHOUSE_URL} --port=${CLICKHOUSE_PORT} -d ${CLICKHOUSE_DATABASE} --user=${CLICKHOUSE_USER} --password=${CLICKHOUSE_PASSWORD} --query"
+CLICKHOUSE_CLIENT="clickhouse-client --host=${CLICKHOUSE_SERVER} --port=${CLICKHOUSE_PORT} -d ${CLICKHOUSE_DATABASE} --user=${CLICKHOUSE_USER} --password=${CLICKHOUSE_PASSWORD} --query"
 
 DB_VERSION=`$CLICKHOUSE_CLIENT "SELECT MAX(version) FROM migrations"`
 
 case $DB_VERSION in
   0|1|2|3)
-    for table_name in `$CLICKHOUSE_CLIENT "SHOW TABLES" | grep -P "^${CLICKHOUSE_LOGS_TABLE}\d+'`; do
+    echo "Start migration to new schema"
+    for table_name in `$CLICKHOUSE_CLIENT "SHOW TABLES" | grep -P "^${CLICKHOUSE_LOGS_TABLE}\d+"`; do
       echo $table_name
       $CLICKHOUSE_CLIENT "INSERT INTO ${CLICKHOUSE_LOGS_TABLE} SELECT * FROM ${table_name}"
       $CLICKHOUSE_CLIENT "DROP TABLE IF EXISTS ${table_name}"
