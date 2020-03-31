9d3740cd93e3dc25639ae4a1bf5f2c1f9a2a978e
Nikolay Bogdanov <nikolay.bogdanov@flant.com>
2020-03-31 18:07:38 +0300

    Fix migration script

--- images/fluentd/migrate-tables.sh
+++ images/fluentd/migrate-tables.sh
@@ -6,31 +6,34 @@ CLICKHOUSE_CLIENT="clickhouse-client --host=${CLICKHOUSE_SERVER} --port=${CLICKH
 
 DB_VERSION=`$CLICKHOUSE_CLIENT "SELECT MAX(version) FROM migrations"`
 
+function migrate_v3 {
+    for table_name in `$CLICKHOUSE_CLIENT "SHOW TABLES" | grep -P "^${CLICKHOUSE_LOGS_TABLE}\d+"`; do
+      echo "Processing ${table_name}..."
+      $CLICKHOUSE_CLIENT "INSERT INTO ${CLICKHOUSE_LOGS_TABLE} SELECT * FROM ${table_name}"
+      $CLICKHOUSE_CLIENT "DROP TABLE IF EXISTS ${table_name}"
+    done
+}
+
+function migrate_v4 {
+  if [[ ! -z `$CLICKHOUSE_CLIENT "SHOW CREATE TABLE ${CLICKHOUSE_LOGS_TABLE}_old" 2>/dev/null` ]]; then
+    for PART_NAME in `$CLICKHOUSE_CLIENT "SELECT DISTINCT(date) FROM ${CLICKHOUSE_LOGS_TABLE}_old"`; do
+      echo "Processing ${PART_NAME}..."
+      $CLICKHOUSE_CLIENT "INSERT INTO ${CLICKHOUSE_LOGS_TABLE} SELECT * FROM ${CLICKHOUSE_LOGS_TABLE}_old WHERE date = '${PART_NAME}'"
+      $CLICKHOUSE_CLIENT "ALTER TABLE ${CLICKHOUSE_LOGS_TABLE}_old DROP PARTITION ${PART_NAME}"
+    done
+    $CLICKHOUSE_CLIENT "DROP TABLE IF EXISTS ${CLICKHOUSE_LOGS_TABLE}_old"
+  fi
+}
+
+echo "Start migration to new schema"
+
 case $DB_VERSION in
   0|1|2|3)
-    echo "Start migration to new schema"
-    for table_name in `$CLICKHOUSE_CLIENT "SHOW TABLES" | grep -P "^${CLICKHOUSE_LOGS_TABLE}\d+"`; do
-      echo "Processing ${table_name}..."
-      $CLICKHOUSE_CLIENT "INSERT INTO ${CLICKHOUSE_LOGS_TABLE} SELECT * FROM ${table_name}"
-      $CLICKHOUSE_CLIENT "DROP TABLE IF EXISTS ${table_name}"
-    done
+    migrate_v3
     ;;
   4)
-    echo "Start migration to new schema"
-    OLD_TABLE=`$CLICKHOUSE_CLIENT "SHOW TABLES" | grep -P "^${CLICKHOUSE_LOGS_TABLE}_old\$"`
-    if [[ ! -z ${OLD_TABLE} ]]; then
-      for PART_NAME in `$CLICKHOUSE_CLIENT "SELECT DISTINCT(date) FROM ${OLD_TABLE}"`; do
-        echo "Processing ${PART_NAME}..."
-        $CLICKHOUSE_CLIENT "INSERT INTO ${CLICKHOUSE_LOGS_TABLE} SELECT * FROM ${OLD_TABLE} WHERE date = '${PART_NAME}'"
-        $CLICKHOUSE_CLIENT "ALTER TABLE ${OLD_TABLE} DROP PARTITION ${PART_NAME}"
-      done
-      $CLICKHOUSE_CLIENT "DROP TABLE IF EXISTS ${OLD_TABLE}"
-    fi
-    for table_name in `$CLICKHOUSE_CLIENT "SHOW TABLES" | grep -P "^${CLICKHOUSE_LOGS_TABLE}\d+"`; do
-      echo "Processing ${table_name}..."
-      $CLICKHOUSE_CLIENT "INSERT INTO ${CLICKHOUSE_LOGS_TABLE} SELECT * FROM ${table_name}"
-      $CLICKHOUSE_CLIENT "DROP TABLE IF EXISTS ${table_name}"
-    done
+    migrate_v3
+    migrate_v4
     ;;
   *)
     echo "Unknown db version $DB_VERSION"
